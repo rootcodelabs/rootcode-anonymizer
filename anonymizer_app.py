a73ff5d34c03eb7e580fd7353fe14635a403ac93
pamodaDilranga <pdrox9@gmail.com>
2024-04-07 23:05:02 +0530

    adding progress bar

--- anonymizer_app.py
+++ anonymizer_app.py
@@ -1,72 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import csv
-# from process_controller import NERProcessorController
-# import codecs
-# import time
-
-# def preprocess_bytes(content):
-#     try:
-#         decoded_content = content.decode('windows-1252').splitlines()
-#         return decoded_content
-#     except UnicodeDecodeError:
-#         print("Unicode replacement")
-#         return codecs.decode(content, 'windows-1252', 'ignore').splitlines()
-
-# def main():
-#     st.title('Rootcode Anonymizer')
-#     print("0")
-
-#     uploaded_file = st.file_uploader("Upload CSV file", type=['csv'])
-#     print("1")
-
-#     file_name = ""
-#     last_processed_file_name = None
-
-#     if uploaded_file is not None:
-#         file_name = uploaded_file.name+"_"+str(int(time.time()))
-#         print("2")
-#         st.write("Uploaded file details:")
-#         st.write(uploaded_file.name)
-#         st.write("Preprocessing CSV file...")
-        
-#         print("3")
-
-#         content = uploaded_file.getvalue()
-
-        
-#         print("4")
-#         preprocessed_content = preprocess_bytes(content)
-
-#         process_controller = NERProcessorController()
-
-#         reader = csv.reader(preprocessed_content)
-#         rows = [row for row in reader]
-#         modified_rows = process_controller.process_sentence_list(rows)
-        
-#         print("5")
-#         st.success("CSV file preprocessed and processed successfully!")
-
-#         print("6")
-#         df = pd.DataFrame(modified_rows)
-
-#         print("7")
-#         st.subheader("Download processed CSV file")
-#         csv_file = df.to_csv(index=False).encode('windows-1252')
-#         st.download_button(
-#             label="Download",
-#             data=csv_file,
-#             file_name='processed_data.csv',
-#             mime='text/csv'
-#         )
-        
-#         print("8")
-#         last_processed_file_name = file_name
-
-# if __name__ == "__main__":
-#     main()
-
-
 import streamlit as st
 import pandas as pd
 import csv
@@ -86,6 +17,9 @@ def preprocess_bytes(content):
         print("Unicode replacement")
         return codecs.decode(content, 'windows-1252', 'ignore').splitlines()
 
+def progress_bar_handler(progress_bar, progress, progress_text):
+    progress_bar.progress(progress, text=progress_text)
+
 def main():
     st.title('Rootcode Anonymizer')
 
@@ -102,6 +36,9 @@ def main():
             st.write(uploaded_file.name)
             st.write("Preprocessing CSV file...")
 
+            progress_text = "Operation in progress. Please wait."
+            my_bar = st.progress(0, text=progress_text)
+
             content = uploaded_file.getvalue()
             preprocessed_content = preprocess_bytes(content)
 
@@ -109,7 +46,7 @@ def main():
 
             reader = csv.reader(preprocessed_content)
             rows = [row for row in reader]
-            modified_rows = process_controller.process_sentence_list(rows)
+            modified_rows = process_controller.process_sentence_list(rows, my_bar)
 
             st.success("CSV file preprocessed and processed successfully!")
 
--- process_controller.py
+++ process_controller.py
@@ -3,6 +3,7 @@ from ner_processer import NERProcessor
 from entity_group import EntityGrouper
 from regex_processor import RegexStringProcessor
 import json
+import anonymizer_app
 
 class NERProcessorController:
     def __init__(self):
@@ -10,7 +11,17 @@ class NERProcessorController:
         self.entity_grouper = EntityGrouper()
         self.regex_processor = RegexStringProcessor()
 
-    def process_sentence_list(self, sentence_rows_list):
+    def calculate_progress_increment(self, list_size):
+        if list_size == 0:
+            return 1
+
+        increment = 99 / list_size  # Calculate the increment for each element
+
+        return increment    
+
+    def process_sentence_list(self, sentence_rows_list, progress_bar):
+        increment = self.calculate_progress_increment(len(sentence_rows_list))
+        
         immutable_words = []
         try:
             with open('immutable_words.json', 'r', encoding='windows-1252') as f:
@@ -21,7 +32,11 @@ class NERProcessorController:
             print(f"Error: {e}. Assigning empty list to immutable_words.")
 
         modified_sentence_rows_list = []
-        for row in sentence_rows_list:
+        count = 0
+        for index, row in enumerate(sentence_rows_list, start=1):
+            count = count + increment
+            progress_text = f'Processing {index} out of {len(sentence_rows_list)}'
+            anonymizer_app.progress_bar_handler(progress_bar, int(count), progress_text)
             modified_row = []
             for sentence in row:
                 sentence  = self.regex_processor.apply_regex_substitution(sentence)
@@ -40,4 +55,5 @@ class NERProcessorController:
                 modified_row.append(final_output)
                 print(modified_row)
             modified_sentence_rows_list.append(modified_row)
+        anonymizer_app.progress_bar_handler(progress_bar, 100, "Done")
         return modified_sentence_rows_list
